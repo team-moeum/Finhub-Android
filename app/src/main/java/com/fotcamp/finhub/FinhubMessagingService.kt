870809ef6ce92b0d527abdb104333173d86c2f83
chomki32 <chomki32@gmail.com>
2024-04-30 23:25:12 +0900

    Feat: Click Push Notification

--- app/src/main/java/com/fotcamp/finhub/FinhubMessagingService.kt
+++ app/src/main/java/com/fotcamp/finhub/FinhubMessagingService.kt
@@ -6,7 +6,6 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.media.RingtoneManager
-import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
@@ -24,21 +23,23 @@ class FinhubMessagingService: FirebaseMessagingService() {
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
 
-        val title = message.notification?.title ?: ""
-        val body = message.notification?.body ?: ""
+        val title = message.data["title"] ?: ""
+        val body = message.data["body"] ?: ""
 
-        createNotification(title, body)
+        createNotification(title, body, message.data)
     }
 
-    private fun createNotification(title: String, body: String) {
+    private fun createNotification(title: String, body: String, data: Map<String, String>) {
         val intent = Intent(this, MainActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        intent.putExtra("push", true)
+        intent.putExtra("view", data["view"] ?: "")
 
         val pendingIntent = PendingIntent.getActivity(
             this,
             0,
             intent,
-            PendingIntent.FLAG_IMMUTABLE)
+            PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
 
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
         val builder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
