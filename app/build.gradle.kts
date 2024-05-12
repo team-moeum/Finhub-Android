df097252b126fbd5dcf2ace92fb6ee6c69243ced
seungdeok <akk9132@naver.com>
2024-05-12 18:21:34 +0900

    chore: Add pr review slack bots

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.fotcamp.finhub"
         minSdk = 30
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 3
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
