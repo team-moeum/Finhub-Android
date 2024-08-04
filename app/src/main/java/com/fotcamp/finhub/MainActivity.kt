3926648516e30fd107e8820c1b0f967566eb90bd
chomki32 <chomki32@gmail.com>
2024-08-04 16:11:09 +0900

    Feat: Add userAgent for google login

--- app/src/main/java/com/fotcamp/finhub/MainActivity.kt
+++ app/src/main/java/com/fotcamp/finhub/MainActivity.kt
@@ -39,6 +39,7 @@ class MainActivity : AppCompatActivity() {
             webSettings.useWideViewPort = true
             webSettings.textZoom = 100;
             webSettings.domStorageEnabled = true
+            webSettings.userAgentString = "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1"
 
             webView.webViewClient = WebViewClient()
             webView.webChromeClient = WebChromeClient()
